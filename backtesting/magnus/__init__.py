ae99e4e4f60423d07b5d92ce28733b03eae0cd34
zuongthao <zuongthao@gmail.com>
2020-10-23 10:18:32 +0700

    update code

--- backtesting/magnus/__init__.py
+++ backtesting/magnus/__init__.py
@@ -1,9 +1,10 @@
 """Data and utilities for testing."""
 import pandas as pd
 
-
-def _read_file(filepth):
-    return pd.read_csv(filepth, index_col=1, parse_dates=True, infer_datetime_format=True)
+def _read_file(filename):
+    from os.path import dirname, join
+    file_path = join(dirname(dirname(dirname(__file__))) , 'method/cophieu68/'+filename)
+    return pd.read_csv(file_path, index_col=1, parse_dates=True, infer_datetime_format=True)
 
 def SMA(arr: pd.Series, n: int) -> pd.Series:
     """
@@ -11,11 +12,11 @@ def SMA(arr: pd.Series, n: int) -> pd.Series:
     """
     return pd.Series(arr).rolling(n).mean()
 
-def _read_file02(filename):
-    from os.path import dirname, join
-    print(join(dirname(__file__), filename))
-    # return pd.read_csv(join(dirname(__file__), filename),
-    #                    index_col=0, parse_dates=True, infer_datetime_format=True)
-
-BID = _read_file02('BID.csv')
-"""DataFrame of daily VNINDEX:BID stock price data from 2014 to 2020."""
+# def _read_file02(filename):
+#     from os.path import dirname, join
+#     print(join(dirname(__file__), filename))
+#     # return pd.read_csv(join(dirname(__file__), filename),
+#     #                    index_col=0, parse_dates=True, infer_datetime_format=True)
+#
+# BID = _read_file02('BID.csv')
+# """DataFrame of daily VNINDEX:BID stock price data from 2014 to 2020."""
--- testcases/StockBreakOut/test01.py
+++ testcases/StockBreakOut/test01.py
@@ -7,30 +7,30 @@ import method.TakeProfit as TakeProfit
 import method.CutLoss as CutLoss
 
 path = os.getcwd()
-class FollowTheTrend(Strategy):
+class StockBreakOut(Strategy):
     def init(self):
         self.buy_price = 0
         self._index = 0
     def next(self):
         prices = self.data.Close
-        opens = self.data.Open
-        volumes = self.data.Volume
-        if len(self.data.Volume) > 5:
+        if len(self.data.Volume) > 66:
             last_price = prices[-1]
-            if self.buy_price == 0 and Ticker.isFollowTrendingV2(prices, volumes, opens[-1], 2.5):
+            if self.buy_price == 0 and Ticker.isStockOut(prices):
                 self.buy_price = last_price
                 self.buy()
             if self.buy_price != 0 and (TakeProfit.takeProfit(5, last_price, self.buy_price) or CutLoss.shouldCutLossByPercent(8, last_price, self.buy_price)):
                 self.position.close()
                 self.buy_price = 0
-vn30_ticker = Ticker.getListVN30()
-for ticker in vn30_ticker:
-    BID = _read_file(ticker+'.csv')
-    bt = Backtest(BID, FollowTheTrend, commission=.005, exclusive_orders=False)
-    stats = bt.run()
-    # bt.plot()
-    print(ticker)
-    print(stats['Sharpe Ratio'])
-    # # print(stats['_trades'])
-    # new_file = path+"/testcases/FollowTheTrend/result_"+ticker+".csv"
-    # stats['_trades'].to_csv(new_file, index=False)
+# vn30_ticker = Ticker.getListVN30()
+# for ticker in vn30_ticker:
+ticker = 'BID'
+ticker_data = _read_file(ticker+'.csv')
+bt = Backtest(ticker_data, StockBreakOut, commission=.005, exclusive_orders=False)
+stats = bt.run()
+# bt.plot()
+print(stats)
+# print(ticker)
+# print(stats['Sharpe Ratio'])
+# # print(stats['_trades'])
+# new_file = path+"/testcases/FollowTheTrend/result_"+ticker+".csv"
+# stats['_trades'].to_csv(new_file, index=False)
