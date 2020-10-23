d58ffba917180ec3c9da6512a7188ca107afa518
zuongthao <zuongthao@gmail.com>
2020-10-23 10:24:38 +0700

    stock break out

--- testcases/StockBreakOut/test01.py
+++ testcases/StockBreakOut/test01.py
@@ -21,16 +21,15 @@ class StockBreakOut(Strategy):
             if self.buy_price != 0 and (TakeProfit.takeProfit(5, last_price, self.buy_price) or CutLoss.shouldCutLossByPercent(8, last_price, self.buy_price)):
                 self.position.close()
                 self.buy_price = 0
-# vn30_ticker = Ticker.getListVN30()
-# for ticker in vn30_ticker:
-ticker = 'BID'
-ticker_data = _read_file(ticker+'.csv')
-bt = Backtest(ticker_data, StockBreakOut, commission=.005, exclusive_orders=False)
-stats = bt.run()
-# bt.plot()
-print(stats)
-# print(ticker)
-# print(stats['Sharpe Ratio'])
-# # print(stats['_trades'])
-# new_file = path+"/testcases/FollowTheTrend/result_"+ticker+".csv"
-# stats['_trades'].to_csv(new_file, index=False)
+vn30_ticker = Ticker.getListBlueChips2020()
+for ticker in vn30_ticker:
+    ticker_data = _read_file(ticker+'.csv')
+    bt = Backtest(ticker_data, StockBreakOut, commission=.005, exclusive_orders=False)
+    stats = bt.run()
+    # bt.plot()
+    # print(stats)
+    # print(ticker)
+    # print(stats['Sharpe Ratio'])
+    # # print(stats['_trades'])
+    new_file = path+"/result_"+ticker+".csv"
+    stats['_trades'].to_csv(new_file, index=False)
